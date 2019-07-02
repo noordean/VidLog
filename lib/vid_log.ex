5e64bd66b8662cab49acc0494ba4dd621db1b9a2
noordean <ebroyeem90@gmail.com>
2019-07-03 00:19:32 +0100

    generate app with mix

---
+++ lib/vid_log.ex
@@ -0,0 +1,18 @@
+defmodule VidLog do
+  @moduledoc """
+  Documentation for VidLog.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VidLog.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vid_log_test.exs
@@ -0,0 +1,8 @@
+defmodule VidLogTest do
+  use ExUnit.Case
+  doctest VidLog
+
+  test "greets the world" do
+    assert VidLog.hello() == :world
+  end
+end
