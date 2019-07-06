9accf95c9ee5f2f5723e65ec84e4f006c393f94d
noordean <ebroyeem90@gmail.com>
2019-07-06 23:49:24 +0100

    make http request to fetch video

--- lib/vid_log.ex
+++ lib/vid_log.ex
@@ -1,18 +1,24 @@
 defmodule VidLog do
-  @moduledoc """
-  Documentation for VidLog.
-  """
+  def columns do
+    ~w(Channel Title Link PublishedDate) |> Enum.join(",")
+  end
 
-  @doc """
-  Hello world.
+  def fetch_video do
+    {:ok, res} = HTTPoison.get(video_url)
+    {:ok, body} = Poison.decode(res.body, keys: :atoms)
+    %{items: [%{ snippet: video_info}]} = body
 
-  ## Examples
+    [
+      video_info.channelTitle,
+      video_info.title,
+      "https://www.youtube.com/watch?v=#{video_info.resourceId.videoId}",
+      video_info.publishedAt
+    ]
+  end
 
-      iex> VidLog.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def video_url do
+    playlist_id = "UUZ4ixYzp3PGev6UQiAgLzDA"
+    api_key = "AIzaSyCmUHlqB1KrppgWeEGeeZ3S54OOu_jPYcY"
+    "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=#{playlist_id}&maxResults=1&key=#{api_key}"
   end
 end
